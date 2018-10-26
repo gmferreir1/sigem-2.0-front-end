6093d7c3b3f8fbe6cbaf1c49f4fbae42b4f2b928
Guilherme Ferreira <gmferreir1@gmail.com>
2018-10-26 10:50:57 -0200

    Ajustando http para https

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ require('./assets_template')
 require('./assets')
 window._notification = require('./util/notification')
 
-window.URL_API = process.env.NODE_ENV === 'development' ? 'http://localhost:90' : 'http://10.0.10.95:66'
+window.URL_API = process.env.NODE_ENV === 'development' ? 'http://localhost:90' : 'https://10.0.10.95:66'
 
 window.http = require('@/util/http')
 
